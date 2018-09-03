4938f5407a18ea1de4dfe12ef300c6f3bcd4b540
FlatIron <flatiron@Lisas-MacBook-Air.local>
2018-09-03 18:44:32 +0100

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,62 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    song = self.new
+    @@all << song
+    song
+  end
+
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    @@all << name
+    song
+  end
+
+  def self.create_by_name(name)
+    song = self.create
+    song.name = name
+    song
+  end
+
+  def self.find_by_name(name)
+    self.all.find{|song| song.name == name}
+  end
+
+  def self.find_or_create_by_name(name)
+    if !self.find_by_name(name)
+      self.create_by_name(name)
+    else self.find_by_name(name)
+    end
+  end
+
+  def self.alphabetical
+    @@all.sort_by{|song| song.name}
+  end
+
+  def self.new_from_filename(file)
+    rows = file.chomp(".mp3").split(" - ")
+    @artist_name = rows[0]
+    @name = rows[1]
+    song = self.new
+    song.artist_name = @artist_name
+    song.name = @name
+    song
+  end
+
+  def self.create_from_filename(file)
+    rows = file.chomp(".mp3").split(" - ")
+    @artist_name = rows[0]
+    @name = rows[1]
+
+    song = self.create
+    song.name = @name
+    song.artist_name = @artist_name
+    song
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
 end
